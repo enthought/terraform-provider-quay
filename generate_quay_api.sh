61d62e1087434bc6a9646535445ad033890de393
David Debeau <ddebeau@enthought.com>
2024-08-02 13:07:21 -0500

    Add README and GNUmakefile

--- generate_quay_api.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-openapi-generator generate --generator-name go --git-user-id enthought --git-repo-id terraform-provider-quay --additional-properties=packageName=quay_api,isGoSubmodule=true --skip-validate-spec --input-spec code_generator/quay_api.json --output quay_api
-gofmt -w quay_api
-cd quay_api
-go mod tidy
