d0fb60c8e140f8d494b21e1097f7d88eb49f1264
David Debeau <ddebeau@enthought.com>
2024-08-19 14:04:05 -0500

    Update docs

---
+++ examples/resources/quay_organization_robot/import.sh
@@ -0,0 +1,2 @@
+# An organization robot can be imported using its short name.
+terraform import quay_organization_robot.test test
--- examples/resources/quay_organization_team/resource.tf
+++ examples/resources/quay_organization_team/resource.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    quay = {
-      source = "registry.terraform.io/enthought/quay"
-    }
-  }
-}
-
-provider "quay" {
-  url = "https://quay.example.com"
-}
-
 resource "quay_organization" "main" {
   name  = "main"
   email = "quay+main@example.com"
