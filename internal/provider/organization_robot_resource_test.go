4a5abffca29fb49519e0cfd65c3c4d4c11c9dc8b
David Debeau <ddebeau@enthought.com>
2024-08-19 11:09:37 -0500

    Parallelize OrganizationRobot test

--- internal/provider/organization_robot_resource_test.go
+++ internal/provider/organization_robot_resource_test.go
@@ -7,57 +7,57 @@ import (
 )
 
 func TestAccOrganizationRobotResource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
+	resource.ParallelTest(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read
 			{
 				Config: providerConfig + `
-resource "quay_organization" "test" {
-  name = "test"
-  email = "quay+test@example.com"
+resource "quay_organization" "org_robot" {
+  name = "org_robot"
+  email = "quay+org_robot@example.com"
 }
 
 resource "quay_organization_robot" "test" {
   name = "test"
-  orgname = quay_organization.test.name
+  orgname = quay_organization.org_robot.name
   description = "test"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("quay_organization_robot.test", "name", "test"),
-					resource.TestCheckResourceAttr("quay_organization_robot.test", "orgname", "test"),
+					resource.TestCheckResourceAttr("quay_organization_robot.test", "orgname", "org_robot"),
 					resource.TestCheckResourceAttr("quay_organization_robot.test", "description", "test"),
-					resource.TestCheckResourceAttr("quay_organization_robot.test", "fullname", "test+test"),
+					resource.TestCheckResourceAttr("quay_organization_robot.test", "fullname", "org_robot+test"),
 				),
 			},
 			// Import
 			{
 				ResourceName:                         "quay_organization_robot.test",
 				ImportState:                          true,
-				ImportStateId:                        "test+test",
+				ImportStateId:                        "org_robot+test",
 				ImportStateVerify:                    true,
 				ImportStateVerifyIdentifierAttribute: "name",
 			},
 			// Replace resource
 			{
 				Config: providerConfig + `
-resource "quay_organization" "test" {
-  name = "test"
-  email = "quay+test@example.com"
+resource "quay_organization" "org_robot" {
+  name = "org_robot"
+  email = "quay+org_robot@example.com"
 }
 
 resource "quay_organization_robot" "test" {
   name = "test2"
-  orgname = quay_organization.test.name
+  orgname = quay_organization.org_robot.name
   description = "test2"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("quay_organization_robot.test", "name", "test2"),
-					resource.TestCheckResourceAttr("quay_organization_robot.test", "orgname", "test"),
+					resource.TestCheckResourceAttr("quay_organization_robot.test", "orgname", "org_robot"),
 					resource.TestCheckResourceAttr("quay_organization_robot.test", "description", "test2"),
-					resource.TestCheckResourceAttr("quay_organization_robot.test", "fullname", "test+test2"),
+					resource.TestCheckResourceAttr("quay_organization_robot.test", "fullname", "org_robot+test2"),
 				),
 			},
 		},
