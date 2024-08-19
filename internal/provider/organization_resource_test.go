c05320989af51c6c3f4b279b0d353cd0ca703fe0
David Debeau <ddebeau@enthought.com>
2024-08-19 11:15:55 -0500

    Parallelize Organization test

--- internal/provider/organization_resource_test.go
+++ internal/provider/organization_resource_test.go
@@ -7,67 +7,67 @@ import (
 )
 
 func TestAccOrganizationResource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
+	resource.ParallelTest(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read
 			{
 				Config: providerConfig + `
-resource "quay_organization" "test" {
-  name = "test"
-  email = "quay+test@example.com"
+resource "quay_organization" "org" {
+  name = "org"
+  email = "quay+org@example.com"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("quay_organization.test", "name", "test"),
-					resource.TestCheckResourceAttr("quay_organization.test", "email", "quay+test@example.com"),
+					resource.TestCheckResourceAttr("quay_organization.org", "name", "org"),
+					resource.TestCheckResourceAttr("quay_organization.org", "email", "quay+org@example.com"),
 				),
 			},
 			// Import
 			{
-				ResourceName:                         "quay_organization.test",
+				ResourceName:                         "quay_organization.org",
 				ImportState:                          true,
-				ImportStateId:                        "test",
+				ImportStateId:                        "org",
 				ImportStateVerify:                    true,
 				ImportStateVerifyIdentifierAttribute: "name",
 			},
 			// Update name
 			{
 				Config: providerConfig + `
-resource "quay_organization" "test" {
-  name = "test2"
-  email = "quay+test@example.com"
+resource "quay_organization" "org" {
+  name = "org2"
+  email = "quay+org@example.com"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("quay_organization.test", "name", "test2"),
-					resource.TestCheckResourceAttr("quay_organization.test", "email", "quay+test@example.com"),
+					resource.TestCheckResourceAttr("quay_organization.org", "name", "org2"),
+					resource.TestCheckResourceAttr("quay_organization.org", "email", "quay+org@example.com"),
 				),
 			},
 			// Update email
 			{
 				Config: providerConfig + `
-resource "quay_organization" "test" {
-  name = "test2"
-  email = "quay+test2@example.com"
+resource "quay_organization" "org" {
+  name = "org2"
+  email = "quay+org2@example.com"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("quay_organization.test", "name", "test2"),
-					resource.TestCheckResourceAttr("quay_organization.test", "email", "quay+test2@example.com"),
+					resource.TestCheckResourceAttr("quay_organization.org", "name", "org2"),
+					resource.TestCheckResourceAttr("quay_organization.org", "email", "quay+org2@example.com"),
 				),
 			},
 			// Update name and email
 			{
 				Config: providerConfig + `
-resource "quay_organization" "test" {
-  name = "test3"
-  email = "quay+test3@example.com"
+resource "quay_organization" "org" {
+  name = "org3"
+  email = "quay+org3@example.com"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("quay_organization.test", "name", "test3"),
-					resource.TestCheckResourceAttr("quay_organization.test", "email", "quay+test3@example.com"),
+					resource.TestCheckResourceAttr("quay_organization.org", "name", "org3"),
+					resource.TestCheckResourceAttr("quay_organization.org", "email", "quay+org3@example.com"),
 				),
 			},
 		},
