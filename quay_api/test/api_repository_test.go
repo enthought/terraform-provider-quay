bc8de2d0ecf6926605f141b82312f985165c8d12
David Debeau <ddebeau@enthought.com>
2024-08-21 13:40:20 -0500

    Create test for ChangeRepoVisibility

--- quay_api/test/api_repository_test.go
+++ quay_api/test/api_repository_test.go
@@ -15,16 +15,37 @@ func Test_quay_api_RepositoryAPIService(t *testing.T) {
 	apiClient := quay_api.NewAPIClient(configuration)
 
 	t.Run("Test RepositoryAPIService ChangeRepoVisibility", func(t *testing.T) {
+		orgName := "change-repo-visibility"
+		repoName := "test"
 
-		t.Skip("skip test") // remove to run test
+		// Ensure org is destroyed
+		defer func(organization quay_api.ApiDeleteAdminedOrganizationRequest) {
+			_, err := organization.Execute()
+			handleQuayAPIError(t, err)
+		}(apiClient.OrganizationAPI.DeleteAdminedOrganization(context.Background(), orgName))
 
-		var repository string
+		// Create org
+		newOrg := quay_api.NewNewOrg(orgName, orgName+"@example.com")
+		httpRes, err := apiClient.OrganizationAPI.CreateOrganization(context.Background()).Body(*newOrg).Execute()
+		handleQuayAPIError(t, err)
+		assert.Equal(t, 201, httpRes.StatusCode)
 
-		httpRes, err := apiClient.RepositoryAPI.ChangeRepoVisibility(context.Background(), repository).Execute()
+		// Create repo
+		newRepo := quay_api.NewRepo{
+			Repository:  repoName,
+			Visibility:  "private",
+			Namespace:   &orgName,
+			Description: "test",
+		}
+		httpRes, err = apiClient.RepositoryAPI.CreateRepo(context.Background()).Body(newRepo).Execute()
+		handleQuayAPIError(t, err)
+		assert.Equal(t, 201, httpRes.StatusCode)
 
-		require.Nil(t, err)
+		// Change repo visibility
+		newVisibility := quay_api.NewChangeVisibility("public")
+		httpRes, err = apiClient.RepositoryAPI.ChangeRepoVisibility(context.Background(), orgName+"/"+repoName).Body(*newVisibility).Execute()
+		handleQuayAPIError(t, err)
 		assert.Equal(t, 200, httpRes.StatusCode)
-
 	})
 
 	t.Run("Test RepositoryAPIService CreateRepo", func(t *testing.T) {
@@ -162,7 +183,7 @@ func Test_quay_api_RepositoryAPIService(t *testing.T) {
 		// Update repo
 		newRepoUpdate := quay_api.NewRepoUpdate("test2")
 		httpRes, err = apiClient.RepositoryAPI.UpdateRepo(context.Background(), orgName+"/"+repoName).Body(*newRepoUpdate).Execute()
-		require.Nil(t, err)
+		handleQuayAPIError(t, err)
 		assert.Equal(t, 200, httpRes.StatusCode)
 	})
 }
