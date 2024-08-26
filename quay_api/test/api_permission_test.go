3804e7b285642d5ba5b65925ce1055b87e450051
David Debeau <ddebeau@enthought.com>
2024-08-26 10:19:44 -0500

    Create test for DeleteTeamPermissions

--- quay_api/test/api_permission_test.go
+++ quay_api/test/api_permission_test.go
@@ -82,17 +82,52 @@ func Test_quay_api_PermissionAPIService(t *testing.T) {
 	})
 
 	t.Run("Test PermissionAPIService DeleteTeamPermissions", func(t *testing.T) {
+		orgName := "delete-team-perm"
+		teamName := "test"
+		repoName := "test"
 
-		t.Skip("skip test") // remove to run test
+		// Ensure org is destroyed
+		defer func(organization quay_api.ApiDeleteAdminedOrganizationRequest) {
+			_, err := organization.Execute()
+			handleQuayAPIError(t, err)
+		}(apiClient.OrganizationAPI.DeleteAdminedOrganization(context.Background(), orgName))
 
-		var repository string
-		var teamname string
+		// Create org
+		newOrg := quay_api.NewNewOrg(orgName, orgName+"@example.com")
+		httpRes, err := apiClient.OrganizationAPI.CreateOrganization(context.Background()).Body(*newOrg).Execute()
+		handleQuayAPIError(t, err)
+		assert.Equal(t, 201, httpRes.StatusCode)
 
-		httpRes, err := apiClient.PermissionAPI.DeleteTeamPermissions(context.Background(), repository, teamname).Execute()
-
-		require.Nil(t, err)
+		// Create team
+		newTeam := quay_api.TeamDescription{
+			Role:        "member",
+			Description: nil,
+		}
+		httpRes, err = apiClient.TeamAPI.UpdateOrganizationTeam(context.Background(), orgName, teamName).Body(newTeam).Execute()
+		handleQuayAPIError(t, err)
 		assert.Equal(t, 200, httpRes.StatusCode)
 
+		// Create repo
+		newRepo := quay_api.NewRepo{
+			Repository:  repoName,
+			Visibility:  "private",
+			Namespace:   &orgName,
+			Description: "test",
+		}
+		httpRes, err = apiClient.RepositoryAPI.CreateRepo(context.Background()).Body(newRepo).Execute()
+		handleQuayAPIError(t, err)
+		assert.Equal(t, 201, httpRes.StatusCode)
+
+		// Change team permissions
+		newTeamPermission := quay_api.NewTeamPermission("read")
+		httpRes, err = apiClient.PermissionAPI.ChangeTeamPermissions(context.Background(), orgName+"/"+repoName, teamName).Body(*newTeamPermission).Execute()
+		handleQuayAPIError(t, err)
+		assert.Equal(t, 200, httpRes.StatusCode)
+
+		// Delete team permissions
+		httpRes, err = apiClient.PermissionAPI.DeleteTeamPermissions(context.Background(), orgName+"/"+repoName, teamName).Execute()
+		handleQuayAPIError(t, err)
+		assert.Equal(t, 204, httpRes.StatusCode)
 	})
 
 	t.Run("Test PermissionAPIService DeleteUserPermissions", func(t *testing.T) {
